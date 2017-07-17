de8a41cfd23541595765291815733004ba6be48f
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-07-17 11:45:49 +0530

    pulling code for sync

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const webpack = require('webpack');
 const card = require('./webpack.config.card.js');
-// const edit_card = require('./webpack.config.edit_card.js');
+const edit_card = require('./webpack.config.edit_card.js');
 
-module.exports = card;
+module.exports = [
+  card,
+  edit_card
+];
