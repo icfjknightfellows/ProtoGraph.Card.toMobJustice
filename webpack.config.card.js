24cc6f351bf0456abe42dfacb448e072d3ca1128
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-08-07 15:49:25 +0530

    Added webpack uglify compression plugin

--- webpack.config.card.js
+++ webpack.config.card.js
@@ -20,6 +20,9 @@ module.exports = {
         NODE_ENV: JSON.stringify('production')
       }
     }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
   ],
   node: {
     net: 'empty',
